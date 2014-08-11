661d5e08caef3cd252819776b4b9059e795827cf
koebel <kathrinkoebel@h501.aau-conf-2-1b.wlan.aalborguniversitet.dk>
2014-08-11 17:43:28 +0200

    comments added

--- Notify/src/com/example/notify/SendNotification.java
+++ Notify/src/com/example/notify/SendNotification.java
@@ -1,17 +1,15 @@
 package com.example.notify;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.apache.http.Header;
 
-import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,8 +20,6 @@ import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
-import com.parse.Parse;
-import com.parse.ParseObject;
 
 public class SendNotification extends ActionBarActivity {
 
@@ -31,31 +27,39 @@ public class SendNotification extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_send_notification);
+		
+		// Activate Up Button in Action Bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-     // Function for Send-Button (Create Message)
+        // Function for Send-Button (Create Message)
      	Button btn_send = (Button) findViewById(R.id.btn_send);
         btn_send.setOnClickListener (new View.OnClickListener() {
         	public void onClick(View v) {
         		
+        		// check if there is internet connection available
         		if(getConnectivityStatus(getApplicationContext())){
         		
         			// Create Strings from Text Input
         			EditText et_msg = (EditText) findViewById(R.id.txtfield_message);
         			String message = et_msg.getText().toString();
     			
-        			// check if there has been content entered into the form before sending
+        			// before sending, check if the message contains content
         			if (message.length()>0){
         				
+        				// get username that has been entered at login
         				SharedPreferences prefs = getPreferences(MODE_PRIVATE);
     					String user = prefs.getString("user_connected_pk", "1");
     					
+    					// connect to the server
     					AsyncHttpClient client = new AsyncHttpClient();
     					RequestParams params = new RequestParams();
+    					// add data
     					params.put("user", user);
     					params.put("message", message);
-    					
+    					// post it to the server within another activity
     					client.post("http://172.17.36.39:8080/notifications/add", params, new AsyncHttpResponseHandler() {
 							
+    						// if successful, go to the display notification activity
 							@Override
 							public void onSuccess(int arg0, Header[] arg1, byte[] response) {
 								Toast.makeText(getApplicationContext(), new String(response), Toast.LENGTH_LONG);
@@ -63,6 +67,7 @@ public class SendNotification extends ActionBarActivity {
 		    					startActivity(i);
 							}
 							
+							// if not successful, stay on current page, display error message
 							@Override
 							public void onFailure(int arg0, Header[] arg1, byte[] arg2, Throwable arg3) {
 								Toast.makeText(getApplicationContext(), "Network error", Toast.LENGTH_LONG);
@@ -72,7 +77,8 @@ public class SendNotification extends ActionBarActivity {
     					
     					}
         			
-        			// if there is no content for either name or message, message can't be sent
+        			// if there is no content in the message, toast will be displayed to enter message
+        			// it is not possible to send an empty message ;-)
         			else{
         				Toast toast = Toast.makeText(getApplicationContext(), "Enter your message", Toast.LENGTH_LONG);
         				toast.show();
@@ -96,18 +102,7 @@ public class SendNotification extends ActionBarActivity {
 		return true;
 	}
 
-	// original generated code
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		// Handle action bar item clicks here. The action bar will
-//		// automatically handle clicks on the Home/Up button, so long
-//		// as you specify a parent activity in AndroidManifest.xml.
-//		int id = item.getItemId();
-//		if (id == R.id.action_settings) {
-//			return true;
-//		}
-//		return super.onOptionsItemSelected(item);
-//	}
+
 	
 	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -122,12 +117,15 @@ public class SendNotification extends ActionBarActivity {
 //            case R.id.action_settings:
 //                openSettings();
 //                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
     
-    // methods for the Icons in the Action Bar
+    // methods called by the Icons in the Action Bar
     public void openEdit(){
     	Intent i = new Intent(getApplicationContext(),SendNotification.class);
         startActivity(i);
